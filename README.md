4f8943c121aca403c954d7c8c41b55c84d6ac0b8
Ryan Hayward <rhhayward@att.net>
2022-12-23 17:41:06 +0000

    bandwidth limit, bps output

--- README.md
+++ README.md
@@ -7,11 +7,16 @@ installation requirement is `requests`.
 ### Usage
 
 Import as `from progress_download import progress_download`.  This will
-give a `progress_download.download` method.  It takes two mandatory
-arguments: URL and filename to write to.  `chunkSize` may be provided
-as a positive integer.  `preFunction` and `postFunction` may be
-provided as functions which will be called before and after the download
-executes.
+give a `progress_download.download` method.
+
+download arguments:
+* `URL` - mandatory string, url to download from.
+* `filename`  - mandatory string, filename to write to.
+* `chunkSize`  - optional positive integer, defaults to 1/100 of file size
+* `preFunction` and `postFunction` - optional functions to execution
+   before and after download.
+* `bandwidthLimit` - optional positive integer, defaults to `+inf`.
+   download will be throttled to this many bytes per second.
 
 ### Example
 
@@ -20,7 +25,7 @@ from progress_download import progress_download
 
 URL = 'http://u.r.l/file.zip'
 
-### Simple usage
+### Simplest usage
 progress_download.download(URL, './file.zip')
 
 ### Set how much to download at a time.  Default is size of file / 100
@@ -34,5 +39,8 @@ def after():
     print("after")
 
 progress_download.download(URL, './file.zip', preFunction=before, postFunction=after)
+
+### Limit bandwidth to 1 MB/s
+progress_download.download(URL, './file.zip', bandwidthLimit=1024*1024)
 
 ```
--- src/progress_download/progress_download.py
+++ src/progress_download/progress_download.py
@@ -1,7 +1,8 @@
 import requests
 import shutil
+import time
 
-def download(url, target, preFunction=None, postFunction=None, chunkSize=None):
+def download(url, target, preFunction=None, postFunction=None, chunkSize=None, bandwidthLimit=None):
     if preFunction != None:
         preFunction()
 
@@ -10,26 +11,32 @@ def download(url, target, preFunction=None, postFunction=None, chunkSize=None):
         downloadedSoFar=0
         if chunkSize == None:
             chunkSize=int(contentLength/100)
+        startTime = time.time() #### /1000
         with open(target, 'wb') as f:
             for chunk in downloadResponse.iter_content(chunk_size=chunkSize):
                 downloadedSoFar += len(chunk)
                 f.write(chunk)
-                _write_status(downloadedSoFar, contentLength)
+                _write_status(downloadedSoFar, contentLength, startTime)
+                if bandwidthLimit != None:
+                    timeToSleep = downloadedSoFar/bandwidthLimit + startTime - time.time()
+                    if timeToSleep > 0:
+                        time.sleep(timeToSleep)
     print()
 
     if postFunction != None:
         postFunction()
 
-def _write_status(curbytes, maxbytes):
+def _write_status(curbytes, maxbytes, startTime):
     (w,h) =  shutil.get_terminal_size()
-    w -= 20
+    w -= 26
     percent = int(curbytes/maxbytes * 100)
     complete_width = int(curbytes/maxbytes * w)
-    print("[{}{}] {:8s}/{:8s}".format(u"#"*complete_width, "."*(w-complete_width), _sizeof_fmt(curbytes), _sizeof_fmt(maxbytes)), end='\r', flush=True)
+    bps = int(curbytes/(time.time()-startTime))
+    print("[{}{}] {:6s}/{:6s} {:6s}".format(u"#"*complete_width, "."*(w-complete_width), _sizeof_fmt(curbytes), _sizeof_fmt(maxbytes), _sizeof_fmt(bps,'/s')), end='\r', flush=True)
 
 def _sizeof_fmt(num, suffix=""):
     for unit in ["B", "K", "M", "G", "T", "P", "E", "Z"]:
-        if abs(num) < 1024.0:
+        if abs(num) < 1000.0:
             return f"{num:3.1f}{unit}{suffix}"
         num /= 1024.0
     return f"{num:.1f}Yi{suffix}"
